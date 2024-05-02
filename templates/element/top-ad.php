922e3a03bd88d0a1e3412cdc401dc74988caabea
conscience-development <connect@conscience.lk>
2024-05-02 11:16:45 +0530

    Ad

--- templates/element/top-ad.php
+++ templates/element/top-ad.php
@@ -43,7 +43,7 @@
 								<img src="/files/banners/photo/<?=$hbanner->photo_dir;?>/ad_Top_<?=$hbanner->photo?>" alt="<?=$hbanner->title?>" style="
 																							width:  100%;
 																							height: 158.825px;
-																							object-fit: fill;
+																							
 
 								">
 							</div>
