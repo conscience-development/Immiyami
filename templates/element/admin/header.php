bbca2d63436cc53bc2a7efe0192b5e5d681e2628
conscience-development <it21164330@my.sliit.lk>
2024-05-08 12:14:55 +0530

    profile pic of admin done

--- templates/element/admin/header.php
+++ templates/element/admin/header.php
@@ -182,9 +182,15 @@
 
 							</li> -->
 							<li class="nav-item dropdown header-profile">
-								<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
-									<img src="/backend/images/profile/5.jpg" width="20" alt=""/>
-								</a>
+								<?php if($Auth->photo) : ?>
+									<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+									<img src="/files/users/photo/<?=$Auth->photo_dir; ?>/<?= $Auth->photo; ?>" width="20" alt=""/>
+									</a>
+								<?php else :?>
+									<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+										<img src="/backend/images/profile/5.jpg" width="20" alt=""/>
+									</a>
+								<?php endif?>
 								<div class="dropdown-menu dropdown-menu-end">
 									<a href="/users/view/<?php echo $Auth->id;?>" class="dropdown-item ai-icon">
 										<svg id="icon-user2" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
